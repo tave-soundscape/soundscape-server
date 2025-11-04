38d42aacd96a459bcd920c0b4ce2a5362175d5c5
heegu123 <kookminkhg@kookmin.ac.kr>
2025-11-04 12:16:05 +0900

    init project

---
+++ src/main/java/com/soundscape/SoundscapeApplication.java
@@ -0,0 +1,13 @@
+package com.soundscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundscapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundscapeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soundscape/SoundscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundscapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
