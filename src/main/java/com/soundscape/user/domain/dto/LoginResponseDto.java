d724cf3ccacb403f70464f8f03d70d6b2552ed3f
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-06 00:39:11 +0900

    chore: LoginResponseDto에 자체 JWT 추가 계획 TODO 작성 - accessToken/refreshToken 외 커스텀 JWT 확장 필요

--- src/main/java/com/soundscape/user/domain/dto/LoginResponseDto.java
+++ src/main/java/com/soundscape/user/domain/dto/LoginResponseDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public class LoginResponseDto {
     private final String accessToken;
     private final String refreshToken;
+    //TODO: 추후 accessToken, refreshToken 외에 자체 JWT 토큰도 추가
 
     public LoginResponseDto(String accessToken, String refreshToken) {
         this.accessToken = accessToken;
