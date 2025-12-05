377efaebc564366c7cc6a382d897e58bdf954be8
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-06 00:38:32 +0900

    refactor: LoginController 요청 경로 "/" 제거 - /api/v1/auth/ -> /api/v1/auth

--- src/main/java/com/soundscape/user/controller/LoginController.java
+++ src/main/java/com/soundscape/user/controller/LoginController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth/")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class LoginController {
 
