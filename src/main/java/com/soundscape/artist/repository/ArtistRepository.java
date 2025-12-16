8faed5e1d1d7d49bbb5e37b4a78bb4defc63fb2c
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-16 16:12:17 +0900

    refactor: 아티스트 정보는 서버 거치지 않고, 앱에서 바로 보여주는 것으로 수정
    - 아티스트 관련 파일 전부 삭제

--- src/main/java/com/soundscape/artist/repository/ArtistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.soundscape.artist.repository;
-
-import com.soundscape.artist.domain.entity.Artist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArtistRepository extends JpaRepository<Artist, Long> {
-}
