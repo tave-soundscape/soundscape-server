aa8c83c6e8859deb6af9237aa390270d60a56454
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-16 17:26:18 +0900

    fix: EntityNotFoundException 에러 코드 수정 및 생성자 확장

--- src/main/java/com/soundscape/common/exception/EntityNotFoundException.java
+++ src/main/java/com/soundscape/common/exception/EntityNotFoundException.java
@@ -5,10 +5,18 @@ import com.soundscape.common.response.ErrorCode;
 public class EntityNotFoundException extends BaseException {
 
     public EntityNotFoundException() {
-        super(ErrorCode.COMMON_INVALID_PARAMETER);
+        super(ErrorCode.COMMON_ENTITY_NOT_FOUND);
+    }
+
+    public EntityNotFoundException(ErrorCode errorCode) {
+        super(errorCode);
     }
 
     public EntityNotFoundException(String message) {
-        super(message, ErrorCode.COMMON_INVALID_PARAMETER);
+        super(message, ErrorCode.COMMON_ENTITY_NOT_FOUND);
+    }
+
+    public EntityNotFoundException(String message, ErrorCode errorCode) {
+        super(message, errorCode);
     }
 }
