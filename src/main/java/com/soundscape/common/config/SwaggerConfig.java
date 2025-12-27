a10588bdcc8955baf987dbc8f2e1658030b659c4
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-28 02:09:00 +0900

    SwaggerConfig 수정: - 배포서버 요청 주소 추가 - jwt 토큰 설정 추가

--- src/main/java/com/soundscape/common/config/SwaggerConfig.java
+++ src/main/java/com/soundscape/common/config/SwaggerConfig.java
@@ -3,18 +3,43 @@ package com.soundscape.common.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         servers = {
                 @Server(url = "http://localhost:8080", description = "로컬 서버"),
+                @Server(url = "http://3.37.62.220:8080", description = "배포 서버")
         },
         info = @Info(
                 title = "SoundScape API",
-                description = "SoundScape API",
+                description = "SoundScape API<br/><br/>"
+                        + "<b>**프론트엔드 테스트 시 꼭 아래 Servers에서 배포 서버로 설정해주세요**.</b>",
                 version = "v1"
         )
 )
 @Configuration
 public class SwaggerConfig {
+        @Bean
+        public OpenAPI openAPI() {
+                // 1. JWT Security Scheme 정의
+                String jwtSchemeName = "jwt토큰 설정";
+                SecurityScheme securityScheme = new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer") // 스킴 타입 bearer
+                        .bearerFormat("JWT"); // Bearer 포맷은 JWT
+
+                // 2. Security Requirement 정의 (모든 API에 적용)
+                SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+                // 3. Components에 Scheme 추가 및 전체 OpenAPI 객체에 Requirement 적용
+                return new OpenAPI()
+                        .components(new Components().addSecuritySchemes(jwtSchemeName, securityScheme))
+                        .addSecurityItem(securityRequirement);
+        }
 }
