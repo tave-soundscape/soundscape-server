0afe25130d354bc26179aa9f750cc4b637e81523
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-27 12:50:20 +0900

    일단 인증 적용 안하도록 주석처리

--- src/main/java/com/soundscape/common/config/WebConfig.java
+++ src/main/java/com/soundscape/common/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.soundscape.common.config;
 import com.soundscape.common.auth.interceptor.JwtAuthenticationInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,14 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
     private final JwtAuthenticationInterceptor interceptor;
 
     // TODO: 추후 인터셉터 적용 경로 확정 후 주석 해제
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor)
-                .order(1);
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(interceptor)
+//                .order(1);
 //                .addPathPatterns("/**")
 //                .excludePathPatterns(
 //                        "/v3/api-docs/**",
 //                        "/swagger-ui/**",
 //                        "/swagger-ui.html");
-    }
+//    }
 }
