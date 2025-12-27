65af819c1c6723d784a3431428f99296f7ee9d40
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-28 01:37:00 +0900

    feat: 인증 인터셉터 경로 설정

--- src/main/java/com/soundscape/common/config/WebConfig.java
+++ src/main/java/com/soundscape/common/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.soundscape.common.config;
 import com.soundscape.common.auth.interceptor.JwtAuthenticationInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,15 +12,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final JwtAuthenticationInterceptor interceptor;
 
-    // TODO: 추후 인터셉터 적용 경로 확정 후 주석 해제
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(interceptor)
-//                .order(1);
-//                .addPathPatterns("/**")
-//                .excludePathPatterns(
-//                        "/v3/api-docs/**",
-//                        "/swagger-ui/**",
-//                        "/swagger-ui.html");
-//    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/api/v1/auth/login",
+                        "/api/v1/auth/refresh",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html");
+    }
 }
