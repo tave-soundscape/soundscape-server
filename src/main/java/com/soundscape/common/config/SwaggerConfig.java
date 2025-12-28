9b84e054eb9cad2155a8e4e493ef350c67a67989
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-28 17:52:25 +0900

    스웨거 테스트 가이드 문서 추가

--- src/main/java/com/soundscape/common/config/SwaggerConfig.java
+++ src/main/java/com/soundscape/common/config/SwaggerConfig.java
@@ -23,8 +23,12 @@ import org.springframework.context.annotation.Configuration;
                         + "**인증 관련**<br/>"
                         + " - 로그인을 제외한 모든 API는 JWT 토큰 인증이 필요합니다.<br/>"
                         + " - 아래 Authorize 버튼을 누르고 임시 JWT 토큰을 입력해주세요.<br/><br/>"
-                        + "**임시 토큰**: <br/>"
-                        + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzY2ODg3ODcyLCJleHAiOjE3OTg0MjM4NzJ9.SMEJfY6YTcfk3bYbnEoa3JHGlSj2Nm49BvZMWbCqoq-0kGN_5az_3GhuYOeP4-d6",
+                        + "**임시 토큰**(유저 id 1인 테스트 유저의 계정으로 로그인 하는 것과 동일): <br/>"
+                        + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzY2ODg3ODcyLCJleHAiOjE3OTg0MjM4NzJ9.SMEJfY6YTcfk3bYbnEoa3JHGlSj2Nm49BvZMWbCqoq-0kGN_5az_3GhuYOeP4-d6"
+                        + "<br/><br/>**임시 토큰을 사용하지 않고, 로그인 테스트 방법**<br/>"
+                        + "1. api/v1/auth 경로로 GET 요청을 보내 카카오 로그인 URL을 받습니다.<br/>"
+                        + "2. 받은 URL로 접속하여 카카오 로그인 후, 리다이렉트된 주소에서 code 파라미터 값을 복사합니다.<br/>"
+                        + "3. api/v1/auth/login 경로로 POST 요청을 보내고, 응답에서 accessToken 값을 확인합니다.",
                 version = "v1"
         )
 )
