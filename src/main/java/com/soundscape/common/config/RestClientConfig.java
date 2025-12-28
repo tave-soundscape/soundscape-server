25fda44ecaa87d42d32202cf5ef04df647005a56
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-28 17:50:43 +0900

    RestClient 빈 설정

---
+++ src/main/java/com/soundscape/common/config/RestClientConfig.java
@@ -0,0 +1,12 @@
+package com.soundscape.common.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+    @Bean
+    public RestClient restClient() {
+        return RestClient.create();
+    }
+}
