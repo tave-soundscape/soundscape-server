d2bca8077c2c88b206eadc6cba1c9d616e1d9e64
heegu123 <kookminkhg@kookmin.ac.kr>
2025-12-30 14:35:10 +0900

    Review 엔티티 추가

---
+++ src/main/java/com/soundscape/reviews/domain/Review.java
@@ -0,0 +1,45 @@
+package com.soundscape.reviews.domain;
+
+import com.soundscape.user.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "reviews")
+public class Review {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Integer rating; // 유저 Rating 0,1,2,3,4 (매우나쁨 ~ 매우좋음)
+    private String dislikeReason;
+    private String preferredMood;
+    private String lyricsPreference;
+    @Lob
+    private String feedback;
+    private Boolean willReuse;
+
+    @Builder
+    public Review(User user, Integer rating, String dislikeReason, String preferredMood, String lyricsPreference, String feedback, Boolean willReuse) {
+        this.user = user;
+        this.rating = rating;
+        this.dislikeReason = dislikeReason;
+        this.preferredMood = preferredMood;
+        this.lyricsPreference = lyricsPreference;
+        this.feedback = feedback;
+        this.willReuse = willReuse;
+    }
+
+    public void assignUser(User user) {
+        this.user = user;
+    }
+}
