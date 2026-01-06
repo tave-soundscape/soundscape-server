5931f5f0ef08ef882ce8905c74a892fc5f04b624
heegu123 <kookminkhg@kookmin.ac.kr>
2026-01-06 16:39:07 +0900

    스포티파이 api 호출을 한곳에서 담당하도록 분리

---
+++ src/main/java/com/soundscape/playlist/infra/spotify/SpotifyPlaylistClient.java
@@ -0,0 +1,15 @@
+package com.soundscape.playlist.infra.spotify;
+
+import com.soundscape.playlist.infra.dto.SpotifyPlaylistInfo;
+import se.michaelthelin.spotify.model_objects.specification.Playlist;
+
+public interface SpotifyPlaylistClient {
+    // 스포티파이 플레이리스트 생성
+    SpotifyPlaylistInfo createPlaylist(String userId, String playlistName);
+    // 플레이리스트에 트랙 추가
+    void addTracksToPlaylist(String playlistId, String[] trackUris);
+    // 플레이리스트 상세 조회, 반환 타입은 스포티파이 라이브러리의 Playlist 객체
+    Playlist getPlaylistDetails(String playlistId);
+    // 플레이리스트 이름 변경
+    void updatePlaylistName(String playlistId, String newName);
+}
