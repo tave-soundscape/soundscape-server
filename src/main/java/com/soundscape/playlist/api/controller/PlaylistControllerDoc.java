d635ba2d3c7c65dc318980876ff990ecf6488df1
heegu123 <kookminkhg@kookmin.ac.kr>
2026-01-12 16:41:31 +0900

    라이브러리에서 플레이리스트 삭제 api 스웨거 문서 추가

--- src/main/java/com/soundscape/playlist/api/controller/PlaylistControllerDoc.java
+++ src/main/java/com/soundscape/playlist/api/controller/PlaylistControllerDoc.java
@@ -18,6 +18,8 @@ public interface PlaylistControllerDoc {
     CommonResponse<PlaylistResponse> generatePlaylist(@RequestBody PlaylistRequest request);
     @Operation(summary = "플레이리스트 이름 수정 및 라이브러리 저장 API", description = "플레이리스트 이름을 수정하고 사용자의 라이브러리에 저장합니다.")
     CommonResponse savePlaylist(@PathVariable Long playlistId, @RequestBody PlaylistNameUpdateRequest newPlaylistName);
+    @Operation(summary = "플레이리스트 라이브러리에서 삭제 API", description = "사용자의 라이브러리에서 플레이리스트를 삭제합니다.")
+    public CommonResponse unsavePlaylist(@PathVariable Long playlistId);
     @Operation(summary = "유저 플레이리스트 조회 API", description = "사용자 입력값을 바탕으로 스포티파이 플레이리스트를 생성합니다.")
     CommonResponse<SimplePlaylistsResponse> getUserPlaylists(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size);
     @Operation(summary = "플레이리스트 상세 조회 API", description = "특정 플레이리스트의 상세 정보를 조회합니다.")
