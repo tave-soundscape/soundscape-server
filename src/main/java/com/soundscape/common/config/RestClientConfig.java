02f094e4a008b778d51e9d4af30c0792f55404e7
heegu123 <kookminkhg@kookmin.ac.kr>
2026-01-23 18:44:20 +0900

    RestClient 타임아웃 설정

--- src/main/java/com/soundscape/common/config/RestClientConfig.java
+++ src/main/java/com/soundscape/common/config/RestClientConfig.java
@@ -1,12 +1,33 @@
 package com.soundscape.common.config;
+
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+
     @Bean
     public RestClient restClient() {
-        return RestClient.create();
+        RequestConfig config = RequestConfig.custom()
+                .setConnectTimeout(Timeout.ofSeconds(5))
+                .setResponseTimeout(Timeout.ofSeconds(20))
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(config)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+
+        return RestClient.builder()
+                .requestFactory(factory)
+                .build();
     }
 }
