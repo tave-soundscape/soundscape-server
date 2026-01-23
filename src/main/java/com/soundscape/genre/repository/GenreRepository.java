dbacd5351b1678612543661b8c182d208e38e40d
heegu123 <kookminkhg@kookmin.ac.kr>
2026-01-23 15:45:39 +0900

    사용하지 않는 클래스들 삭제

--- src/main/java/com/soundscape/genre/repository/GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.soundscape.genre.repository;
-
-import com.soundscape.genre.domain.entity.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-}
